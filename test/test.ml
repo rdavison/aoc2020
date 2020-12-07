87ad4616030ebbf1d1786d8efbd386b0aa1b51ce
Richard Davison <richard.neil.davison@gmail.com>
2020-12-07 03:57:04 -0500

    Remove annoying 2020 from library/package name

--- test/test.ml
+++ test/test.ml
@@ -1,12 +1,12 @@
 let%expect_test "day 1" =
-  let input = Aoc2020.Day1.of_string [%blob "day/1/input"] in
+  let input = Aoc.Day1.of_string [%blob "day/1/input"] in
   let to_string x =
     match x with
     | None -> "Not found"
     | Some num -> Printf.sprintf "%d" num
   in
-  printf "%s" (Aoc2020.Day1.part1 input |> to_string);
+  printf "%s" (Aoc.Day1.part1 input |> to_string);
   [%expect {| 357504 |}];
-  printf "%s" (Aoc2020.Day1.part2 input |> to_string);
+  printf "%s" (Aoc.Day1.part2 input |> to_string);
   [%expect {| 12747392 |}]
 ;;
