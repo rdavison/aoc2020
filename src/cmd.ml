882fe36640e6a173b8685901f39b2e4ebd7e8723
Richard Davison <richard.neil.davison@gmail.com>
2020-12-07 01:10:54 -0500

    Implement part2, add documentation to mli

--- src/cmd.ml
+++ src/cmd.ml
@@ -3,8 +3,7 @@ let day1 ~dir =
     Stdlib.Filename.concat dir "input" |> In_channel.read_all |> Day1.of_string
   in
   let part1 = Day1.part1 input in
-  (* let part2 = Day1.part2 input in *)
+  let part2 = Day1.part2 input in
   printf "Part One: %d\n" part1;
-  (* printf "Part Two: %d\n" part2 *)
-  ()
+  printf "Part Two: %d\n" part2
 ;;
--- src/day1.ml
+++ src/day1.ml
@@ -9,18 +9,44 @@ let of_string x =
          | line -> Some (Int.of_string line))
 ;;
 
-let adds_to_2020 x y = Int.equal 2020 (x + y)
+let sums_to_2020 l = Int.equal 2020 (List.fold l ~init:0 ~f:( + ))
 
 let rec part1 l =
   match l with
   | [] -> failwith "Not found"
   | x :: rest ->
-    (match List.find rest ~f:(adds_to_2020 x) with
+    (match List.find rest ~f:(fun y -> sums_to_2020 [ x; y ]) with
     | Some y -> x * y
     | None -> part1 rest)
 ;;
 
-let%expect_test "part1 example" =
-  printf "%d" (part1 [ 1721; 979; 366; 299; 675; 1456 ]);
-  [%expect {| 514579 |}]
+let part2 l =
+  let rec loop l =
+    match l with
+    | [] -> None
+    | x :: ys ->
+      (match ys with
+      | [] -> None
+      | y :: zs ->
+        (match List.find zs ~f:(fun z -> sums_to_2020 [ x; y; z ]) with
+        | Some z -> Some (x * y * z)
+        | None ->
+          (match loop (x :: zs) with
+          | Some a -> Some a
+          | None ->
+            (match loop (y :: zs) with
+            | Some a -> Some a
+            | None -> loop zs))))
+  in
+  match l |> List.sort ~compare:Int.compare |> loop with
+  | None -> failwith "Not found"
+  | Some a -> a
+;;
+
+let%expect_test "given examples" =
+  let data = [ 1721; 979; 366; 299; 675; 1456 ] in
+  printf "%d" (part1 data);
+  [%expect {| 514579 |}];
+  printf "%d" (part2 data);
+  [%expect {| 241861950 |}]
 ;;
